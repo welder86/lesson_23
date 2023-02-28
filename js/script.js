a20ca2cfccdffb0b75e7c1a952eca7d910fb20f6
welder86 <112901748+welder86@users.noreply.github.com>
2023-02-28 15:28:24 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,259 @@
+"use strict"
+// console.log('ssdfgf');
+// console.log('Привіт Я тут');
+
+// let old = 36;
+
+// console.log('old');
+
+
+// let size = 400;
+// console.log(size);
+// let size2;
+// size2 = size;
+// console.log(size2);
+
+// const ageOld = 132;
+// console.log(ageOld);
+
+
+// const COLOR_RED = '#498635';
+
+
+/*
+let someString = `Привіт`;
+console.log(someString.length);
+ */
+
+/*
+let firstName = `Welder`;
+console.log(firstName[0]);
+*/
+
+/*
+let someString = `Welder`;
+console.log(someString.toUpperCase());
+console.log(someString.toLowerCase());
+console.log(someString[1].toUpperCase());
+ */
+
+/*
+let firstName = `Welder`;
+let firstNameLower = firstName.toLowerCase();
+console.log(firstNameLower);
+console.log(firstNameLower.includes('w'));
+ */
+
+
+/*
+let firstName = `Welder`;
+let firstNameNew = firstName.slice(0, 4);
+console.log(firstNameNew);
+ */
+
+/*
+let firstName = `Welder`;
+firstName = firstName.replace('W', 'T');
+console.log(firstName);
+ */
+
+// ======== Методи чисел ==========
+
+/*
+let someNum = 30;
+let someNotRoundNum = 30.1;
+console.log(Math.ceil(someNotRoundNum));
+ */
+/*
+console.log(Math.random());
+ */
+
+/*
+console.log(Math.max(2, 5, -10));
+console.log(Math.min(2, 5, -10));
+let numMax = Math.max(2, 6, -9);
+console.log(numMax);
+ */
+
+/*
+let someVar = `35.5px`;
+let someNum = parseFloat(someVar) + 15;
+console.log(someNum);
+ */
+
+/*
+let varOne = 79;
+let varTwo = 5;
+ */
+/* let varSumm = varOne - varTwo;
+console.log(typeof varSumm);
+console.log(varSumm);
+ */
+
+/*
+let varSumm = varOne / varTwo;
+console.log(typeof varSumm);
+console.log(varSumm);
+ */
+
+/*
+let varSumm = varOne  * varTwo;
+console.log(typeof varSumm);
+console.log(varSumm);
+ */
+
+/*
+let varSumm = varOne % varTwo;
+console.log(typeof varSumm);
+console.log(varSumm);
+ */
+
+/*
+let varOne = "79";
+let varTwo = "5";
+
+let varSumm = varOne + varTwo;
+console.log(typeof varSumm);
+console.log(varSumm);
+ */
+/*
+let someNumOne = 7;
+let someNumTwo = 17;
+
+console.log(++someNumTwo);
+console.log(--someNumOne);
+
+ */
+/*
+let results = 2 * 8 + 3 / "7";
+console.log(results);
+ */
+
+/*
+let  results = 5 || 6;
+console.log(results);
+ */
+
+
+/*
+ let varOne = 5;
+let varTwo = 10;
+
+if (varOne > varTwo) {
+    let result = varOne + varTwo;
+    console.log(result);
+} else {
+console.log('Hello');
+}
+*/
+
+
+/*
+let someString = "Welder is beter"
+ */
+/*
+console.log(someString[0]);
+console.log(someString[1]);
+console.log(someString[2]);
+console.log(someString[3]);
+console.log(someString[4]);
+console.log(someString[5]);
+ */
+
+
+/*
+console.log(`К-сть символів: ${someString.length}`);
+
+for (let i = 0; i < someString.length; ++i) {
+    console.log(someString[i]);
+}
+*/
+
+// домашка ==== 1 ===
+
+// варіант 1
+// console.log('35' + - "22");
+// поверне 13 =====   невірно
+
+// варіант 2
+// console.log('35' * "22");
+// поверне 770 ====  вірно
+
+// варіант 3
+// console.log('558' > 22++);
+// виведе true === невірно
+
+// варіант 4
+// let usersCounter = 0;
+// let newUsers = usersCounter++;
+// console.log(newUsers);
+// поверне 1 ==== невірно
+
+// варіант 5
+// console.log(!false && 11 || 18 && !'');
+// поверне 11
+
+// варіант 6
+// let name = 0;
+// console.log(name ?? "Без назви");
+
+
+// домашка ===== 2 =====
+
+// варіант 1
+/* if (1 === "1") {
+    console.log('Правда');
+}else {
+    console.log('Брехня');
+} */
+
+// варіант 2
+/* if (5 == "5") {
+    console.log('Правда');
+}else {
+    console.log('Брехня');
+} */
+
+// варіант 3
+/* console.log(92 > '11' && 58 < 100) ? 'Правда' : 'Брехня';
+ */
+
+// варіант 4
+/* if (0) {
+    console.log('Брехня');
+} else if (" ") {
+    console.log('Правда');
+}
+ */
+
+// домашка  ==== 3 ====
+/* 
+for (let num = 1; num < 6; num++) {
+    console.log(num);
+}
+ */
+
+/* 
+let num = 8;
+
+while (num) {
+    console.log(num);
+    num--;
+}
+ */
+
+
+/*
+
+for (let num = 0; num < 3; num++) {
+    console.log(`Число: ${num}`);
+}
+ */
+
+/* 
+let num = 0;
+while (num < 3) {
+    console.log(`Число: ${num}`);
+    num++;
+}
+ */
